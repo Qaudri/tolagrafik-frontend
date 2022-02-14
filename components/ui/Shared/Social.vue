c96faac30ebac41fd4ae4b5c619c15941afa143a
Qaudri <aquadirmuhammad@gmail.com>
2022-02-14 16:16:44 +0100

    refactor: home page issues fixed

--- components/ui/Shared/Social.vue
+++ components/ui/Shared/Social.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="flex justify-evenly lg:hidden pb-4 ">
+  <div class="">
     <div class="flex">
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/dribbble.svg')" alt="" width="25px">
       </div>
 
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/behance.svg')" alt="" width="25px" >
       </div>
 
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/linkedin.svg')" alt="" width="25px">
       </div>
 
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/facebook_f.svg')" alt="" width="25px">
       </div>
 
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/instagram.svg')" alt="" width="25px">
       </div>
 
-      <div class="bg-gray-400 p-2 rounded-full m-2">
+      <div class="bg-gray-400 p-2 hover:bg-yellow-500 transition ease-in-out duration-700 rounded-full m-2">
         <img :src="require('@/assets/images/twitter.svg')" alt="" width="25px">
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -11,32 +11,39 @@
         <img src="@/assets/images/tola-pic.png" alt="" width="100%" class="mt-14">
       </div>
 
-      <div class="flex md:py-4 md:px-10 px-8 py-6 lg:w-1/2 lg:absolute lg:bottom-0 lg:right-0 2xl:px-16">
+      <div class="flex w-full">
 
-        <!-- <div class="w-6/12"></div> -->
-        <p class="2xl:text-2xl font-normal 2xl:leading-8 text-white text-sm md:text-2xl md:leading-8 lg:leading-7 lg:text-xl">
+        <div class="w-1/2 hidden lg:block"></div>
+
+        <div class="md:py-4 md:px-10 px-8 py-6 lg:w-1/2 lg:object-none lg:object-right 2xl:px-16">
+
+          <p class="2xl:text-2xl font-normal 2xl:leading-8 text-white text-sm md:text-2xl md:leading-8 lg:leading-7 lg:text-xl">
           
-          Hi, I'm <span class="text-yellow-400">Omotola</span>!
-            <br>
-          My focus is to help businesses achieve desired visibility and growth, through the use of professional logo designs that resonates with their target audience.
-            <br />
-          With more than 500 satisfied clients across various sectors, your brand value matters and I am confident that you will find me resourceful as a designer to layout your ideas and keep you ahead of the game.
-            <br />
-          In clarity and simplicity paired with distinctive aesthetic, your brand is positioned to benefit from innovative solutions that I bring to bare.
-            <br />
-          I express a high level of professionalism, knowing that the right visual communications will no doubt lead to profitable growth, selling the right messages to the right people and at all times.
-            <br />
-          I enjoy working with start-ups, SMEs and businesses of all sizes. It gives me great pleasure to be part of your success stories.
-            <br />
-          Let's get ahead together!
-            <br />
-          Thank you.
-        </p>
+            Hi, I'm <span class="text-yellow-400">Omotola</span>!
+              <br>
+            My focus is to help businesses achieve desired visibility and growth, through the use of professional logo designs that resonates with their target audience.
+              <br />
+            With more than 500 satisfied clients across various sectors, your brand value matters and I am confident that you will find me resourceful as a designer to layout your ideas and keep you ahead of the game.
+              <br />
+            In clarity and simplicity paired with distinctive aesthetic, your brand is positioned to benefit from innovative solutions that I bring to bare.
+              <br />
+            I express a high level of professionalism, knowing that the right visual communications will no doubt lead to profitable growth, selling the right messages to the right people and at all times.
+              <br />
+            I enjoy working with start-ups, SMEs and businesses of all sizes. It gives me great pleasure to be part of your success stories.
+              <br />
+            Let's get ahead together!
+              <br />
+            Thank you.
+          </p>
+
+        <UiSharedSocial class="my-3 hidden lg:block" />
+        </div>
+
       </div>
     </div>
 
     <div>
-      <UiSharedSocial />
+      <UiSharedSocial class="flex justify-evenly pb-4  lg:hidden" />
     </div>
   </div>
   </div>
